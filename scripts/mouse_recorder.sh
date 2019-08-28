ccc81d64e677198fc7c6b4885a36a3d15e8d17c5
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-08-28 10:40:54 +0200

    Update (scripts/mouse_recorder.sh): renamed run.sh into mouse_recorder.sh

--- scripts/mouse_recorder.sh
+++ scripts/mouse_recorder.sh
@@ -1,5 +1,6 @@
 
 # To run this scripts is necessary to have already installed python3.*, pip3 and venv using pip3
+# Is necessary to pass a name for the user to record
 
 python3 -m venv env
 . env/bin/activate
