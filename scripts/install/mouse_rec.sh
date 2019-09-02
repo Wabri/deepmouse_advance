0fe8db1c6eb02d634be8355e767ac1bbf9c0e89b
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-09-02 09:43:22 +0200

    Update (mouse_rec.sh & mouse_rec.spec): move mouse_rec.spec into mouse_recorder packages and update mouse_rec.sh

--- scripts/install/mouse_rec.sh
+++ scripts/install/mouse_rec.sh
@@ -1,5 +1,5 @@
-
 echo 'Setting up virtual environment'
+rm -rf env
 python3 -m venv env
 echo 'Activate environment'
 source env/bin/activate
@@ -9,6 +9,8 @@ echo 'Install requirements of mouse_recorder'
 pip install -r packages/mouse_recorder/requirements.txt
 echo 'Install pyinstaller'
 pip install pyinstaller
+echo 'Remove Useless files and folders'
+rm -rf build dist
 echo 'Create mouse_rec executable'
-pyinstaller --onefile installer/mouse_rec.spec
+pyinstaller --onefile packages/mouse_recorder/mouse_rec.spec
 
