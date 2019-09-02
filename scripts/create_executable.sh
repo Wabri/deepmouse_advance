5e664d8d352efeea345f273a39732d761eac3104
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-09-02 09:55:25 +0200

    Update (mouse_recorder.sh): add some echo output to track the executions

--- scripts/create_executable.sh
+++
@@ -1,8 +0,0 @@
-
-# necessary python3 and venv
-# create a virtual environment env
-# activate it and run:
-#
-
-pyinstaller --onefile packages/mouse_recorder/mouse_recorder/mouse_rec.py -p env/lib/python3.7/site-packages/ --hiddenimport pymouse
-
--- scripts/mouse_recorder.sh
+++ scripts/mouse_recorder.sh
@@ -1,8 +1,14 @@
-
-# To run this scripts is necessary to have already installed python3.*, pip3 and venv using pip3
-# Is necessary to pass a name for the user to record
-
+echo 'Setting up virtual environment'
+rm -rf env
 python3 -m venv env
+echo 'Activate Environment'
 . env/bin/activate
-pip3 install --upgrade pip
+echo 'Upgrade pip'
+pip install --upgrade pip
+echo 'Intall requirements of mouse_recorder'
+pip install -r packages/mouse_recorder/requirements.txt
+echo 'Run mouse_recorder with python3 and 2 arguments'
+PYTHONPATH=./packages/mouse_recorder python3 packages/mouse_recorder/mouse_recorder/mouse_rec.py $1 $2
+echo 'Deactivate environment'
+deactivate
 
