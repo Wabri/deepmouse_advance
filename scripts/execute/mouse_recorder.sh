4b9406e4938f23ef3ac857e754c564fd9d41ca55
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-09-02 11:19:15 +0200

    Update (scripts/*): create new echo function

--- scripts/execute/mouse_recorder.sh
+++ scripts/execute/mouse_recorder.sh
@@ -1,33 +1,36 @@
 #!/bin/sh
 
-separator=-------------------------
+_sep_echo(){
+	echo '---------------------------'
+	echo $1
+}
 
 package_path=packages/mouse_recorder
 environment_path=$package_path/env
 main_file=$package_path/mouse_recorder/mouse_rec.py
 
-echo $separator
-echo 'Setting up virtual environment'
+_sep_echo $separator
+_sep_echo 'Setting up virtual environment'
 rm -rf $environment_path
 python3 -m venv $environment_path
-echo $separator
-echo 'Activate Environment'
+_sep_echo $separator
+_sep_echo 'Activate Environment'
 . $environment_path/bin/activate
-echo $separator
-echo 'Upgrade pip'
+_sep_echo $separator
+_sep_echo 'Upgrade pip'
 pip install --upgrade pip
-echo $separator
-echo 'Intall requirements of mouse_recorder'
+_sep_echo $separator
+_sep_echo 'Intall requirements of mouse_recorder'
 pip install -r $package_path/requirements.txt
-echo $separator
-echo 'Run mouse_recorder with python3 and 2 arguments'
+_sep_echo $separator
+_sep_echo 'Run mouse_recorder with python3 and 2 arguments'
 PYTHONPATH=./$package_path python3 $main_file $1 $2
-echo $separator
-echo 'Deactivate environment'
+_sep_echo $separator
+_sep_echo 'Deactivate environment'
 deactivate
-echo $separator
-echo 'Remove environment'
+_sep_echo $separator
+_sep_echo 'Remove environment'
 rm -rf $environment_path
 
-echo $separator
+_sep_echo $separator
 
--- scripts/install/mouse_rec.sh
+++ scripts/install/mouse_rec.sh
@@ -1,43 +1,45 @@
 #!/bin/sh
 
-separator=-------------------------
+_sep_echo(){
+	echo '---------------------------'
+	echo $1
+}
 
 package_path=packages/mouse_recorder
 environment_path=$package_path/env
 
-echo $separator
-echo 'Setting up virtual environment'
+_sep_echo 'Setting up virtual environment'
 rm -rf $environment_path
 python3 -m venv $environment_path
-echo $separator
-echo 'Activate environment'
+_sep_echo $separator
+_sep_echo 'Activate environment'
 . $environment_path/bin/activate
-echo $separator
-echo 'Upgrade pip'
+_sep_echo $separator
+_sep_echo 'Upgrade pip'
 pip install --upgrade pip
-echo $separator
-echo 'Install requirements of mouse_recorder'
+_sep_echo $separator
+_sep_echo 'Install requirements of mouse_recorder'
 pip install -r $package_path/requirements.txt
-echo $separator
-echo 'Install pyinstaller'
+_sep_echo $separator
+_sep_echo 'Install pyinstaller'
 pip install pyinstaller
-echo $separator
-echo 'Remove Useless files and folders'
+_sep_echo $separator
+_sep_echo 'Remove Useless files and folders'
 rm -rf build dist
-echo $separator
-echo 'Create mouse_rec executable'
+_sep_echo $separator
+_sep_echo 'Create mouse_rec executable'
 pyinstaller --onefile $package_path/mouse_rec.spec
-echo $separator
-echo 'Deactivate virtual environment'
+_sep_echo $separator
+_sep_echo 'Deactivate virtual environment'
 deactivate
-echo $separator
-echo 'Remove environment'
+_sep_echo $separator
+_sep_echo 'Remove environment'
 rm -rf $environment_path
-echo $separator
-echo 'Remove build and move executable'
+_sep_echo $separator
+_sep_echo 'Remove build and move executable'
 rm -rf mouse_rec.run
 mv dist/mouse_rec ./mouse_rec.run
 rm -rf build dist
 
-echo $separator
+_sep_echo $separator
 
