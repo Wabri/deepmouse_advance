6eda35890eca06caec5de9a8ee776b3b57488495
Wabri <gabriele.puliti@protonmail.com>
2019-09-05 15:18:33 +0200

    Update (scripts/*): documentation for pip and development_setup

--- scripts/development_setup/source.sh
+++ scripts/development_setup/source.sh
@@ -5,8 +5,10 @@ _sep_echo(){
 	echo $1
 }
 
-environment_path=./venv
-scripts_path=./scripts
+root_project=$(pwd)
+packages_path=$1
+environment_path=$packages_path/venv
+scripts_path=$root_project/scripts
 
 _sep_echo 'Deactivate precedent environment'
 deactivate
@@ -18,19 +20,18 @@ source $environment_path/bin/activate
 _sep_echo 'Upgrade pip'
 pip install --upgrade pip
 _sep_echo 'Install requirements of mouse_recorder'
-pip install -r ./requirements.txt
-
+pip install -r $packages_path/requirements.txt
 _sep_echo 'Setting up alias command'
 _sep_echo 'You can use the run command to execute python packages run.py'
 echo '          run <name_package> [<arguments>]'
-alias run='sh $scripts_path/execute/run.sh'
+alias run='cd $root_project ; sh $scripts_path/execute/run.sh'
 _sep_echo 'You can use the make_exe command to create an executable file with pyinstaller'
 echo '          make_exe <name_package>'
 echo 'The new file will be created in the root folder'
-alias make_exe='sh $scripts_path/install/install.sh'
+alias make_exe='cd $root_project ; sh $scripts_path/install/install.sh'
 _sep_echo 'You can use the requirements command to create a requirements.txt of the package'
 echo '          requirements <name_package>'
 echo 'The new file will be created in the package folder'
-alias requirements='sh $scripts_path/pip/requirements.sh'
+alias requirements='cd $root_project ; sh $scripts_path/pip/requirements.sh'
 
 _sep_echo ''
--- scripts/pip/requirements.sh
+++ scripts/pip/requirements.sh
@@ -6,13 +6,26 @@ _sep_echo(){
 }
 
 package_path=packages/$1
+environment_path=packages/$1/env
 requirements_file_name=$package_path/requirements.txt
 
+_sep_echo 'Deactivate precedent (if exists) environment'
+deactivate
+_sep_echo 'Setting up virtual environment'
+rm -rf $environment_path
+python3 -m venv $environment_path
+_sep_echo 'Activate Environment'
+source $environment_path/bin/activate
+_sep_echo 'Upgrade pip'
+pip install --upgrade pip
+_sep_echo 'Install requirements of package '$1
+pip install $2
 _sep_echo 'Freeze requirements'
 pip freeze > $requirements_file_name
 _sep_echo 'Remove first line of freeze'
 sed 1d $requirements_file_name > $package_path/temp
 _sep_echo 'Move requirements into '$package_path
 mv $package_path/temp $requirements_file_name
+
 _sep_echo ''
 
