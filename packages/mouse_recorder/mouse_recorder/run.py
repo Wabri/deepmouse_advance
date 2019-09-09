0cfa9dba2d5b6873ac5c1e44b849242a69d580b2
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-09-09 18:21:58 +0200

    Fix (configuration)

--- packages/mouse_recorder/mouse_recorder/run.py
+++ packages/mouse_recorder/mouse_recorder/run.py
@@ -8,26 +8,43 @@ from mouse_recorder.config.config import Config
 import signal
 import time
 
-config = Config()
+configuration = Config()
 stop = Event()
 
+for arg in sys.argv[2:]:
+    arguments = str(arg).split(sep='=')
+    print(arguments)
+    argument = arguments[0].lstrip('--').upper()
+    try:
+        value = arguments[1]
+    except:
+        value = True
+    if hasattr(configuration, argument):
+        print('Change {} from {}'.format(argument, getattr(configuration, argument)),end=' ')
+        setattr(configuration, argument, value)
+        print('to {}'.format(getattr(configuration, argument)))
+    else:
+        print("Argument {} don't have references".format(argument))
+configuration.reset_type()
+
+
 def _merge(
-        dataset_path='{}/{}'.format(config.DATASET_PATH, config.USERNAME),
-        filename_template=config.FILENAME_TEMPLATE,
-        files_extension_template=config.FILES_EXTENSION_TEMPLATE,
-        remove_same=config.REMOVE_SAME,
+        username=configuration.USERNAME,
+        dataset_path=configuration.DATASET_PATH,
+        filename_template=configuration.FILENAME_TEMPLATE,
+        files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+        remove_same=True,
         ordered=True,
-        file_output=config.USERNAME):
+        file_output=configuration.USERNAME):
     """
     """
-    files_path = '{}/*.{}'.format(dataset_path, files_extension_template)
-    files = glob(files_path)
+    files = glob('{}/*'.format(dataset_path))
 
     if ordered:
-        key = lambda x:int(x.lstrip('{}/{}'.format(dataset_path,filename_template)).rstrip('.'+files_extension_template))
-        files = sorted(glob(files_path), key=key)
+        key = lambda x:int(x.split('/')[-1].rstrip('.{}'.format(files_extension_template)).lstrip('{}'.format(filename_template)))
+        files = sorted(glob('{}/{}/*'.format(dataset_path,username)), key=key)
 
-    with open('{}.{}'.format(file_output, files_extension_template), 'w') as merge:
+    with open(file_output, 'w') as merge:
         last_line = ''
         for file in files:
             with open(file,'r') as dataset:
@@ -39,26 +56,43 @@ def _merge(
                     else:
                         merge.write('{}'.format(line))
 
-    with open('{}.{}'.format(file_output, files_extension_template), 'r') as merged:
+    with open(file_output, 'r') as merged:
         print('The merged file lines are: {}'.format(len(merged.readlines())))
 
 
 def _merge_datas(
-        dataset_path='{}/{}'.format(config.DATASET_PATH, config.USERNAME),
-        filename_template=config.FILENAME_TEMPLATE,
-        files_extension_template=config.FILES_EXTENSION_TEMPLATE,
-        output_file_name=config.USERNAME,
-        output_extension=config.FILES_EXTENSION_TEMPLATE,
-        sleep_time=config.SLEEP_TIME):
+        username=configuration.USERNAME,
+        dataset_path=configuration.DATASET_PATH,
+        filename_template=configuration.FILENAME_TEMPLATE,
+        files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+        output_file_name=configuration.USERNAME,
+        output_extension=configuration.FILES_EXTENSION_TEMPLATE,
+        sleep_time=configuration.SLEEP_TIME):
     """
     """
     time.sleep(sleep_time)
     if os.path.exists(dataset_path):
         print('Dataset path {}/'.format(dataset_path))
-        print('With removing same')
-        _merge(dataset_path=dataset_path, filename_template=filename_template, files_extension_template=files_extension_template, remove_same=True, ordered=True, file_output='{}'.format(output_file_name, output_extension))
-        print('With same')
-        _merge(dataset_path=dataset_path, filename_template=filename_template, files_extension_template=files_extension_template, remove_same=False, ordered=True, file_output='{}_ws'.format(output_file_name,output_extension))
+        print('With removing same and work with ordered')
+        _merge(
+            username=username,
+            dataset_path=dataset_path,
+            filename_template=filename_template,
+            files_extension_template=files_extension_template,
+            remove_same=True,
+            ordered=True,
+            file_output='{}.{}'.format(output_file_name,output_extension)
+        )
+        print('With same and work with ordered')
+        _merge(
+            username=username,
+            dataset_path=dataset_path,
+            filename_template=filename_template,
+            files_extension_template=files_extension_template,
+            remove_same=False,
+            ordered=True,
+            file_output='{}_withsame.{}'.format(output_file_name,output_extension)
+        )
     else:
         print('The path does not exist')
 
@@ -83,10 +117,10 @@ def _get_last_number_file(files, regex_pattern):
 
 
 def _increment_filename(
-        dataset_path='{}/{}'.format(config.DATASET_PATH,config.USERNAME),
-        filename_template=config.FILENAME_TEMPLATE,
-        files_extension_template=config.FILES_EXTENSION_TEMPLATE,
-        max_digit=len(str(config.MAX_ITERATIONS))):
+        dataset_path='{}/{}'.format(configuration.DATASET_PATH,configuration.USERNAME),
+        filename_template=configuration.FILENAME_TEMPLATE,
+        files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+        max_digit=len(str(configuration.MAX_ITERATIONS))):
     """
     """
     files_extension_template = '.' + files_extension_template
@@ -106,24 +140,31 @@ def _increment_filename(
 
 
 def run_mouse_recorder(
-        username=config.USERNAME,
-        filename_template=config.FILENAME_TEMPLATE,
-        files_extension_template=config.FILES_EXTENSION_TEMPLATE,
-        max_iterations=config.MAX_ITERATIONS,
-        point_per_file=config.POINT_PER_FILE,
-        sleep_time=config.SLEEP_TIME):
+        username=configuration.USERNAME,
+        dataset_path=configuration.DATASET_PATH,
+        filename_template=configuration.FILENAME_TEMPLATE,
+        files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+        max_iterations=configuration.MAX_ITERATIONS,
+        point_per_file=configuration.POINT_PER_FILE,
+        sleep_time=configuration.SLEEP_TIME):
     """
     """
-    if not os.path.exists('./dataset/{}'.format(username)):
-        os.makedirs('./dataset/{}'.format(username))
-    dataset_path = './dataset/{}/'.format(username)
+    dataset_path = '{}/{}'.format(dataset_path, username)
+    if not os.path.exists(dataset_path):
+        os.makedirs(dataset_path)
+    dataset_path = '{}/'.format(dataset_path)
 
     mouse = PyMouse()
     run = 0
 
     while (max_iterations < 0 or run < max_iterations) and not stop.is_set():
         print('Recording run {}'.format(run))
-        record_filename = _increment_filename()
+        record_filename = _increment_filename(
+            dataset_path,
+            filename_template,
+            files_extension_template,
+            len(str(max_iterations))
+        )
         run += 1
         with open(record_filename, 'w') as rf:
             for index in range(point_per_file):
@@ -138,29 +179,50 @@ def run_mouse_recorder(
 
 def interruption(sig, _frame):
     stop.set()
-    if config.MERGE_FILES:
-        _merge_datas()
+    if configuration.MERGE_FILES:
+        _merge_datas(
+            configuration.USERNAME,
+            configuration.DATASET_PATH,
+            configuration.FILENAME_TEMPLATE,
+            configuration.FILES_EXTENSION_TEMPLATE,
+            configuration.OUTPUT_FILE_MERGE,
+            configuration.OUTPUT_EXTENSION,
+            configuration.SLEEP_TIME
+        )
 
 if __name__ == '__main__':
 
-    for arg in sys.argv[1:]:
-        arguments = str(arg).split(sep='=')
+    if configuration.ONLY_MERGE:
+        _merge_datas(
+            username=configuration.USERNAME,
+            dataset_path=configuration.DATASET_PATH,
+            filename_template=configuration.FILENAME_TEMPLATE,
+            files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+            output_file_name=configuration.OUTPUT_FILE_MERGE,
+            output_extension=configuration.OUTPUT_EXTENSION,
+            sleep_time=configuration.SLEEP_TIME
+        )
+        exit()
+
+    for file in glob(configuration.DATASET_PATH+'/*'):
         try:
-            argument = arguments[0].lstrip('--').upper()
-            try:
-                value = arguments[1]
-            except:
-                value = True
-            if getattr(config, argument):
-                setattr(config, argument, value)
-            else:
-                print("Argument {} don't have references".format(argument))
-        except Exception as e:
-            pass
+            files_extension_template = '.' + configuration.FILES_EXTENSION_TEMPLATE
+            files = glob(os.path.join(configuration.DATASET_PATH, configuration.FILENAME_TEMPLATE + '*.' + configuration.FILES_EXTENSION_TEMPLATE))
+            for file in files:
+                print(int(file.lstrip('{}/{}'.format(configuration.DATASET_PATH,configuration.FILENAME_TEMPLATE)).rstrip('.'+configuration.FILES_EXTENSION_TEMPLATE)))
+        except:
+            print(file)
 
     signal.signal(signal.SIGINT, interruption)
 
-    run_mouse_recorder()
+    run_mouse_recorder(
+        username=configuration.USERNAME,
+        filename_template=configuration.FILENAME_TEMPLATE,
+        files_extension_template=configuration.FILES_EXTENSION_TEMPLATE,
+        max_iterations=configuration.MAX_ITERATIONS,
+        point_per_file=configuration.POINT_PER_FILE,
+        sleep_time=configuration.SLEEP_TIME
+    )
 
     if not stop.is_set():
         os.kill(os.getpid(), signal.SIGINT)
