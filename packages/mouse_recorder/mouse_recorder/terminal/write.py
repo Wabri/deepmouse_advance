56cf54102ca7656ab59bbf7f434987df4a0d84c3
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-09-17 11:20:39 +0200

    Update (terminal module): fix some movement and print

--- packages/mouse_recorder/mouse_recorder/terminal/write.py
+++ packages/mouse_recorder/mouse_recorder/terminal/write.py
@@ -5,16 +5,26 @@ def move_up(times=1):
         print('',end='\x1b[A')
 
 
+def move_down(times=1):
+    for _ in range(times):
+        print('',end='\x1b[B')
+
+
 def clear_line(end='\r'):
     characters = os.get_terminal_size().columns
     print(' '*characters, end=end)
 
 
+def clear_lines(lines):
+    for _ in range(lines):
+        clear_line(end='\r')
+        move_up()
+
+
 def clear_all():
     lines = os.get_terminal_size().lines
-    for _ in range(lines):
-        clear_line(end='\n')
-    move_up(lines)
+    move_down()
+    clear_lines(lines)
 
 
 def progress_bar(current_value, max_value, title='', leave_title=False):
@@ -25,9 +35,9 @@ def progress_bar(current_value, max_value, title='', leave_title=False):
         progress = 100
     clear_line()
     bar = '[{0}{1}] {2:.1f}%'.format('#'*int(progress/2), ' ' * (50-int(progress/2)), progress)
-    output_string = title + '\n' + bar
+    output_string = ((title + '\n') if not title == '' else '')  + bar
     print(output_string, end='')
-    move_up()
+    if not title == '': move_up()
     print('', end='\r')
     if leave_title:
         print(title)
