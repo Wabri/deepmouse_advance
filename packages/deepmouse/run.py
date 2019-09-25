76b5cc3d3a8c91cec5a91a43703e1f1bb0225dbd
Gabriele Puliti <gabriele.puliti@protonmail.com>
2019-09-25 12:14:54 +0200

    Update (packages/deepmouse/run.py)

--- packages/deepmouse/run.py
+++ packages/deepmouse/run.py
@@ -2,12 +2,12 @@ import pymouse
 import time
 
 
-def save_to_file(x, y, file):
+def save_mouse_position_to_file(x, y, file):
     with open(file, 'a') as append_file:
         print('Mouse Position: {}, {}'.format(x, y), file=append_file, end='\n')
 
 
-def load_from_file(file):
+def load_mouse_position_from_file(file):
     with open(file, 'r') as read_file:
         line = read_file.readlines()[-1]
         print(line, end='')
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     while True:
         mouse = pymouse.PyMouse()
         x, y = mouse.position()
-        save_to_file(x, y, 'test.txt')
+        save_mouse_position_to_file(x, y, 'test.txt')
         time.sleep(1)
-        load_from_file('test.txt')
+        load_mouse_position_from_file('test.txt')
